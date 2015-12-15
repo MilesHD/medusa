678f4381d82e02ec49d7c4265c6d758bf9064ecc
Miles Dickinson <miles.dickinson@ge.com>
2015-12-15 12:33:16 -0500

    Add initial page structure and interactivity

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
 (function() {
     'use strict';
+
+    var nodes = [
+        {id: 1, label: ''},
+        {id: 2, label: ''},
+        {id: 3, label: ''},
+        {id: 4, label: ''}
+    ];
+
+    var edges = [
+        {to: 1, from: 3},
+        {to: 1, from: 2},
+        {to: 2, from: 4},
+    ];
     
-    console.log('hi');
+    function renderGraph(nodes, edges) {
+        nodes = new vis.DataSet(nodes);
+        edges = new vis.DataSet(edges);
+
+        var container = document.getElementById('vis');
+
+        var data = {
+            nodes: nodes,
+            edges: edges
+        };
+
+        var options = {};
+
+        var network = new vis.Network(container, data, options)
+
+    }
+
+    // Event handlers
+    $('#submit-query').on('click', function submitQuery(evt) {
+        evt.preventDefault()
+        alert(evt);
+    });
+
+    $('#default-query').on('click', function defaultQuery(evt) {
+        evt.preventDefault()
+        alert(evt);
+    });
+
+
+    // Initial Render
+    renderGraph(nodes, edges);
 
 }());
