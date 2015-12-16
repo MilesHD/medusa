cc8730bd5cfeae0d18ac26114ef85c12480f2b01
Miles Dickinson <miles.dickinson@ge.com>
2015-12-16 12:18:09 -0500

    Add logo, UI logic and validation for query controls

--- main.js
+++ main.js
@@ -33,16 +33,42 @@
 
     // Event handlers
     $('#submit-query').on('click', function submitQuery(evt) {
+        evt.preventDefault()
+
+        // Collect, sanitize, and validate input
+        var reference = $('#reference').val().trim();
+        var daysBack = $('#days-back').val().trim();
+        var sources = $('input[name=sources]:checked').map(function() {
+            return $(this).val();
+        });
+
+        if (reference === undefined || reference === "") {
+            alert("Please enter a reference");
+            return;
+        }
+        if (daysBack === undefined || daysBack === "") {
+            alert("Please enter the number of days");
+            return;
+        }
+        if (sources.length === 0) {
+            alert("Please select at least 1 source");
+            return;
+        }
         // Ajax Call to Alertweb server
         // Needs to be some controller / route handler to communicate
         // with python script
-        evt.preventDefault()
-        alert(evt);
+        console.log(reference, daysBack, sources);
+        
     });
 
-    $('#default-query').on('click', function defaultQuery(evt) {
+    // Clear form controls
+    $('#clear-query').on('click', function clearQuery(evt) {
         evt.preventDefault()
-        alert(evt);
+        $('#reference').val("");
+        $('#days-back').val("");
+        $('input[name=sources]:checked').each(function() {
+            return $(this).prop('checked', false);
+        });
     });
 
 
